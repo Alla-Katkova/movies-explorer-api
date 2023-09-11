e464bef506c47e2b9ea5d5aed535242d0a2ec423
Kot <katkova.alla@gmail.com>
2023-09-11 20:05:19 +0300

    создала централизацию ошибок в app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+// require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/moviesdb' } = process.env;
 
 const app = express();
+
+// логгер
+app.use(requestLogger);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,5 +21,25 @@ mongoose.connect(DB_URL, {
 });
 
 app.use('/users', require('./routes/users'));
+
+// логгер ошибок
+app.use(errorLogger);
+
+// для работы celebrate
+app.use(errors());
+
+// централизация ошибок
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        // ? 'На сервере произошла ошибка'
+        ? JSON.stringify(err)
+        : message,
+    });
+  next();
+});
 
 app.listen(PORT);
