d5b3da7645557534c1f5d77bc6af63391abfcbb8
Kot <katkova.alla@gmail.com>
2023-09-11 17:30:50 +0300

    создание схем для user и movie, установка остальных пакетов

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Заполните поле'],
+    minlength: [2, 'Минимальное количество символов - 2'],
+    maxlength: [30, 'Максимальное количество символов - 30'],
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Заполните поле'],
+    unique: [true, 'Email уже существуют'],
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+        // return /^\S+@\S+\.\S+$/.test(email);
+      },
+      message: 'Введите верный email или пароль',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Заполните поле'],
+    select: false,
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('user', userSchema);
