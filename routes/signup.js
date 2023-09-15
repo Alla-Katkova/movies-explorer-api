925f5d8a61f0556a57e619d41b8ea74a508798fe
Kot <katkova.alla@gmail.com>
2023-09-16 00:14:54 +0300

    feat: создан файлы для роутов sign in /up

--- routes/signup.js
+++ routes/signup.js
@@ -1,16 +1,13 @@
-// const router = require('express').Router();
-// const { celebrate, Joi } = require('celebrate');
-// const { createUser } = require('../controllers/users');
-// const urlRegex = require('../utils/constants');
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { createUser } = require('../controllers/users');
 
-// router.post('/', celebrate({
-//   body: Joi.object().keys({
-//     name: Joi.string().min(2).max(30),
-//     about: Joi.string().min(2).max(30),
-//     avatar: Joi.string().pattern(urlRegex),
-//     email: Joi.string().required().email(),
-//     password: Joi.string().required().min(5),
-//   }).unknown(true),
-// }), createUser);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(3),
+  }).unknown(true),
+}), createUser);
 
-// module.exports = router;
+module.exports = router;
