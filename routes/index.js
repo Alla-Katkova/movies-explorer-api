086683292959ab4d46deb7411f1fc77da20102f9
Kot <katkova.alla@gmail.com>
2023-09-16 00:17:25 +0300

    feat: в index.js внела данные для sign in /up

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,15 @@ const NotFoundError = require('../errors/NotFoundError');
 
 router.use('/signup', signupRouter);
 router.use('/signin', signinRouter);
-// const auth = require('../middlewares/auth');
+const auth = require('../middlewares/auth');
 
 // router.use(auth);
 
 router.use('/users', usersRouter);
 router.use('/movies', moviesRouter);
 
-// app.use('/signup', require('./routes/signup'));
-// app.use('/signin', require('./routes/signin'));
+app.use('/signup', require('./routes/signup'));
+app.use('/signin', require('./routes/signin'));
 
 // если запрос идет на неизвестный роут
 router.use('*', (req, res, next) => {
